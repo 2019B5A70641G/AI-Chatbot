f106000ccd9c1ba79d3c1c50879a6150f5a23994
2019B5A70641G <125313098+2019B5A70641G@users.noreply.github.com>
2024-07-08 21:46:25 +0530

    Update app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -8,7 +8,7 @@ config();
 const app = express();
 
 //middlewares
-app.use(cors({origin: "https://ai-chatbot-frontend-8d71.onrender.com",credentials: true}));
+app.use(cors({origin: true ,credentials: true}));
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 //remove it before production
